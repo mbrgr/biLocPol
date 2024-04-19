bace1d713f5fd3c81a30d468168bf9d7fbedc528
mbrgr <max.j.ber96@gmail.com>
2024-04-19 14:30:42 +0200

    Added test to check if vector or matrix gets returned

--- tests/testthat/test-local_polynomial_weights.R
+++ tests/testthat/test-local_polynomial_weights.R
@@ -26,6 +26,17 @@ testthat::test_that("correct dimension", {
                  dim(), c(p*(p-1), 6))
 })
 
+testthat::test_that("return type", {
+  p = 20
+  d_grid = observation_grid(p, comp = "less")
+  expect_true( is.vector(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 1, del = 0)))
+  expect_true( is.vector(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 2, del = 0)))
+  expect_true( is.matrix(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 1, del = 1)))
+  expect_true( is.matrix(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 2, del = 2)))
+
+})
+
+
 testthat::test_that("warning for small bandwidth", {
   p = 5
   d_grid = observation_grid(p, comp = "less")
