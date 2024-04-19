fdbe19d6bc7e43d3d55fd36223fb82061cbf433e
mbrgr <max.j.ber96@gmail.com>
2024-04-19 14:26:25 +0200

    changed test to check whether weights_point return a vector in the case where del = 0

--- tests/testthat/test-local_polynomial_weights.R
+++ tests/testthat/test-local_polynomial_weights.R
@@ -4,21 +4,22 @@ testthat::test_that("correct dimension", {
   p = 20
   d_grid = observation_grid(p, comp = "less")
   expect_equal(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 1, del = 0) |>
-                 dim(), c(1, p*(p-1)/2))
+                 length(), p*(p-1)/2)
   expect_equal(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 1, del = 1) |>
                  dim(), c(p*(p-1)/2, 3))
   expect_equal(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 2, del = 2) |>
                  dim(), c(p*(p-1)/2, 6))
+
   d_grid = observation_grid(p, comp = "full")
   expect_equal(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 2, del = 0) |>
-                 dim(), c(1, p^2))
+                 length(), p^2)
   expect_equal(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 2, del = 1) |>
                  dim(), c(p^2, 3))
   expect_equal(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 2, del = 2) |>
                  dim(), c(p^2, 6))
   d_grid = observation_grid(p, comp = "without diagonal")
   expect_equal(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 1, del = 0) |>
-                 dim(), c(1, p*(p-1)))
+                 length(), p*(p-1))
   expect_equal(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 1, del = 1) |>
                  dim(), c(p*(p-1), 3))
   expect_equal(weights_point(x = c(0.2, 0.3), d_grid, h = 0.2, K = epak_2d, m = 2, del = 2) |>
